7c1af28596b6dc28ae22e5f755ee28c1ec06df1a
Andres Florez <anflo.97@gmail.com>
2019-01-17 16:28:49 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word1,word2;
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
