959a5d13c7baff2a6ce7bf9e3d0143c92a6e7468
Andres Florez <anflo.97@gmail.com>
2019-01-17 16:57:18 -0500

    PASSED IN HACKER RANK

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,104 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String word1,word2;
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-	}
-
+    public static boolean isAnagram(String a, String b) {
+        char[] word1 = a.toLowerCase().toCharArray();
+        char[] word2 = b.toLowerCase().toCharArray();
+        sort(word1, 0, word1.length-1);
+        sort(word2, 0, word2.length-1);
+        if(word1.length != word2.length)return false;
+        boolean flag = true;
+        for (int i = 0; i < word1.length; i++) {
+            if (word1[i] != word2[i])
+                flag = false;
+        }
+        if (flag) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+   static void merge(char arr[], int l, int m, int r) 
+        { 
+            // Find sizes of two subarrays to be merged 
+            int n1 = m - l + 1; 
+            int n2 = r - m; 
+      
+            /* Create temp arrays */
+            char L[] = new char [n1]; 
+            char R[] = new char [n2]; 
+      
+            /*Copy data to temp arrays*/
+            for (int i=0; i<n1; ++i) 
+                L[i] = arr[l + i]; 
+            for (int j=0; j<n2; ++j) 
+                R[j] = arr[m + 1+ j]; 
+      
+      
+            /* Merge the temp arrays */
+      
+            // Initial indexes of first and second subarrays 
+            int i = 0, j = 0; 
+      
+            // Initial index of merged subarry array 
+            int k = l; 
+            while (i < n1 && j < n2) 
+            { 
+                if (L[i] <= R[j]) 
+                { 
+                    arr[k] = L[i]; 
+                    i++; 
+                } 
+                else
+                { 
+                    arr[k] = R[j]; 
+                    j++; 
+                } 
+                k++; 
+            } 
+      
+            /* Copy remaining elements of L[] if any */
+            while (i < n1) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+                k++; 
+            } 
+      
+            /* Copy remaining elements of R[] if any */
+            while (j < n2) 
+            { 
+                arr[k] = R[j]; 
+                j++; 
+                k++; 
+            } 
+        } 
+      
+        // Main function that sorts arr[l..r] using 
+        // merge() 
+        static void sort(char arr[], int l, int r) 
+        { 
+            if (l < r) 
+            { 
+                // Find the middle point 
+                int m = (l+r)/2; 
+      
+                // Sort first and second halves 
+                sort(arr, l, m); 
+                sort(arr , m+1, r); 
+      
+                // Merge the sorted halves 
+                merge(arr, l, m, r); 
+            } 
+        } 
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
