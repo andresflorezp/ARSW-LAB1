deaf4b39d76f4120dc3b4a585a07467381d50349
andres97 <anflo.97@gmail.com>
2019-01-19 21:23:14 -0500

    modificando a ruta absoluta

--- MAVEN/file-spy/src/main/java/edu/eci/FileSpy.java
+++ MAVEN/file-spy/src/main/java/edu/eci/FileSpy.java
@@ -7,8 +7,10 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 public class FileSpy {
 	private static final String FILE_TYPE = "text/csv";
 	private static final String DIR_TO_WATCH = "C:\\Users\\2121990\\Documents\\ARSW_PRUEBA\\file-spy\\resources";
+	private static final String DIR_TO_W = "C:\\Users\\Andres\\Documents\\final\\ARSW-LAB1\\MAVEN\\file-spy\\src\\main\\java\\edu\\eci\\resources";
+	private static final String FINAL = new java.io.File("").getAbsolutePath()+"\\src\\main\\java\\edu\\eci\\resources";
 	public static void main(String[] args) throws Exception {
-        Path dir = Paths.get("src/main/java/edu/eci/resources");
+		Path dir = Paths.get(FINAL);
         Tika tika = new Tika();
         WatchService watchService = FileSystems.getDefault().newWatchService();
         dir.register(watchService, ENTRY_CREATE);
